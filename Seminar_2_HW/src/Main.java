9bc66cd3181046ca6606d90f2b086a8dc7d39c9e
Anton Sineev <sineevanton@mail.ru>
2022-11-03 17:33:56 +0600

    Orogram structure created in Main class.

---
+++ Seminar_2_HW/src/Main.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        boolean working = true;
+
+        while (working){
+
+            System.out.println("Hello!\nChose the task you want to work with\n" +
+                    "(enter the number 1, 2, 3 or 4)\n" +
+                    "Or type 'q' to stop the program.");
+
+            try {
+                String choice = reader.readLine().toLowerCase();
+                switch (choice){
+                    case ("1"):
+                        System.out.println("1");
+                        break;
+                    case ("2"):
+                        System.out.println("2");
+                        break;
+                    case ("3"):
+                        System.out.println("3");
+                        break;
+                    case ("4"):
+                        System.out.println("4");
+                        break;
+                    case ("q"):
+                        System.out.println("q");
+                        working = false;
+                        break;
+                    default:
+                        System.out.println("Incorrect input. Please, try again.");
+                }
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+
+
+        }
+
+    }
+}
